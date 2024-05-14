61e8a1dad100728d47455569cd89c106f56b1239
Adolberton <7557@holbertonstudents.com>
2024-05-14 14:20:00 +0200

    auto commit

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,9 +2,9 @@
 
 const fs = require('fs');
 const filePath = process.argv[2];
-const ftext = process.argv[3];
+const content = process.argv[3];
 
-fs.writeFile(filePath,  ftext, 'utf-8', (err) => {
+fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.error(err);
   }
