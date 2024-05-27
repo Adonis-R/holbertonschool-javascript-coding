80ec485a11e51f32cfd1bc819aac7fc4c9ab7be5
Adolberton <7557@holbertonstudents.com>
2024-05-27 15:52:36 +0200

    auto commit

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -3,12 +3,11 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n');
+    const lines = data.split('\n').filter(line => line.trim().length > 0);
     lines.shift();
     const studentsByField = {};
 
     for (const line of lines) {
-      if (line.trim().length > 0) {
         const [firstname,,, field] = line.split(',');
 
         if (!studentsByField[field]) {
@@ -16,19 +15,16 @@ function countStudents(path) {
         }
         studentsByField[field].push(firstname);
       }
-    }
     const totalStudents = lines.length;
 
     console.log(`Number of students: ${totalStudents}`);
 
     for (const field in studentsByField) {
-      if (studentsByField) {
         const count = studentsByField[field].length;
         const list = studentsByField[field].join(', ');
         console.log(`Number of students in ${field}: ${count}. List: ${list}`);
       }
-    }
-  } catch (error) {
+    } catch (error) {
     throw new Error('Cannot load the database');
   }
 }
