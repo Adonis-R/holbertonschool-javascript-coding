c935f617a90d5ef76c3801fa618553044a56625b
Adolberton <7557@holbertonstudents.com>
2024-05-27 15:18:12 +0200

    auto commit

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,10 @@
-console.log('Welcome to Holberton School, la mienne est plus grosse!!!what is your name?');
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  process.exit();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
