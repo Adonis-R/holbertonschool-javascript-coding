0cfb6e69a0f655c8a3b39ddcb5cf54bb89f6b358
Adolberton <7557@holbertonstudents.com>
2024-05-30 14:35:10 +0200

    auto commit

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const PORT = 1245;
+const HOST = 'localhost';
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server listening at -> http://${HOST}:${PORT}`);
+});
