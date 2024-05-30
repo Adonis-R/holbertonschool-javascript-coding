d945e41aba9b125dc9ccf5514bd64effb7ddd12c
Adolberton <7557@holbertonstudents.com>
2024-05-30 15:09:50 +0200

    auto commit

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -3,20 +3,20 @@ const fs = require('fs').promises;
 
 const app = express();
 const PORT = 1245;
-const HOST = 'localhost';
+const DB_FILE = process.argv[2];
 
-app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.send('Hello Holberton School!');
-});
-
+/**
+ * Get the students from the database file (CSV format)
+ * @param {string} database
+ * @returns {string}
+ */
 async function getStudents(database) {
   const data = await fs.readFile(database, 'utf8');
   const lines = data.split('\n').filter((line) => line);
   const students = lines.map((line) => line.split(','));
   const csStudents = students.filter((student) => student[3] === 'CS');
   const sweStudents = students.filter((student) => student[3] === 'SWE');
-  const totalStudents = csStudents.length + sweStudents.length;
+  const totalStudents = students.length;
   const totalCsStudents = csStudents.length;
   const totalSweStudents = sweStudents.length;
   const csStudentsList = csStudents.map((student) => student[0]).join(', ');
@@ -29,14 +29,25 @@ async function getStudents(database) {
   ].join('\n');
 }
 
-app.get('/students',async (req, res) => {
-  const mateub = await getStudents(process.argv[2]);
+app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
-  res.status(200).send(`This is the list of our students\n${mateub}`);
+  res.send('Hello Holberton School!');
 });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server listening at -> http://${HOST}:${PORT}`);
+app.get('/students', async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
+  try {
+    const students = await getStudents(DB_FILE);
+    res.write(students);
+  } catch (err) {
+    res.write('Cannot load the database');
+  }
+  res.end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
